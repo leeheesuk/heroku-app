41a2858cc3ef2f57f03b088036b275effca80421
unknown <hoon9405@gmail.com>
2020-12-22 14:17:03 +0900

    upload nodejs project

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = process.env.PROT || 3000;
+
+app.get('/',(req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log('Example app listening on port 3000'));
